34e3abf5114eb973d461b3b97ecffdc18e312536
Sharvagya <sharvagyak@gmail.com>
2023-04-23 21:48:28 +0545

    day 3

--- operators/bin/example2.dart
+++ operators/bin/example2.dart
@@ -1,3 +1,16 @@
 void main(List<String> args) {
   ///There are four types of operators in dart- Unary Prefix,Unary Postfix, Binary Infix, Compound Assignment
+  var age = 30; //const can't be modified using unary
+  // var age2 = --age; //doesnt only change but also returns the value
+  print(--age);
+  // print(age2);
+  print(!true); //this only prints false doesn't change the value of true\
+  print(!false);
+  print(++age); //modifies value of age
+  print(age);
+
+  ///unary bitwise complement prefix operator ~
+  print(~1); //It flips the 32 bit value( changes 0's to 1's and vice versa)
+  print(-age); //doesn't change the value only prints it in minus
+  print(age);
 }
---
+++ operators/bin/example3.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  ///unary postfix operators
+  var age = 40;
+  print(age--); //postfix-age is printed first then 1 is subtracted
+  print(age);
+  print(age++);
+  print(age);
+}
