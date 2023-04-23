6b17aca34a0ea666db78961c36286fbc846d75c7
Sharvagya <sharvagyak@gmail.com>
2023-04-23 22:35:10 +0545

    day 3

---
+++ operators/bin/example5.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  ///type promotion
+  const int1 = 1;
+  const double1 = 1.1;
+  const result = double1 + int1;
+  print(result); //returns in double due to type promotion
+}
---
+++ operators/bin/example6.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  var age = 20;
+  print(age = 30); //compound assignment
+  print(age ~/=
+      2); //if = comes after an operator it assigns the result back to the variable
+  print(age *= 2);
+  print(age -= 4);
+}
